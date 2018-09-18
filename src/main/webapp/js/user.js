397c5f3f272d220bcf55a858f1729e610806dd43
小奇冰 <BenBencyb@users.noreply.github.com>
2018-09-18 15:20:18 +0800

    修改了品牌模块部分显示，首页改善，用户订单中心界面基本完成

--- src/main/webapp/js/user.js
+++ src/main/webapp/js/user.js
@@ -6,34 +6,3 @@ $(document).ready(function(){
 //		$(".tab-box blockquote").eq(tabindex).show().siblings().hide();
 	 })
 })
-
-var app = new Vue({
-  el: '#app2',
-  data: {
-  	test1:'233',
-  	object: {
-  
-   },
-    orders:[
-      { id: '2018090288111' , time:'2018-09-03' , money:'78.00' ,status:'待付款',
-      	href:'index.html' 
-      },
-      { id: '2018090288585' , time:'2018-09-02' , money:'144.00' ,status:'待付款',
-      	href:'index.html' 
-      },
-      { id: '2018090288987' , time:'2018-09-01' , money:'255.00' ,status:'待付款',
-      	href:'index.html' 
-      },
-      { id: '2018090288123' , time:'2018-09-01' , money:'366.00' ,status:'待付款',
-      	href:'index.html' 
-      },
-      { id: '2018090285674' , time:'2018-09-01' , money:'55.00' ,status:'待付款',
-      	href:'index.html' 
-      }
-    ],
-    token:"abc"
-  },
-  methods:{
-  		
-  }
-})
