fabb9d2283ac8d5ac8ee30532454f8155ddaa1c5
your name <your email>
2018-09-29 10:54:39 +0800

    商家查看客户评论详情并回复评论部分功能实现（还差图片显示）

---
+++ src/main/java/cn/myzqu/ygmall/dao/ReplyCommentMapper.java
@@ -0,0 +1,22 @@
+package cn.myzqu.ygmall.dao;
+
+import cn.myzqu.ygmall.pojo.ReplyComment;
+
+public interface ReplyCommentMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(ReplyComment record);
+
+    int insertSelective(ReplyComment record);
+
+    ReplyComment selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(ReplyComment record);
+
+    int updateByPrimaryKey(ReplyComment record);
+
+    ReplyComment selectByCommentId(String commentId);
+
+    int updateContentById(ReplyComment record);
+
+}
