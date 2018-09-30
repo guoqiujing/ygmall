0050060394a1657526df52210935582b5709f5b6
guoqiujing <17876253486@163.com>
2018-09-30 15:40:07 +0800

    修改根据订单id获取订单明细服务层

--- src/main/java/cn/myzqu/ygmall/service/impl/OrderDetailServiceImpl.java
+++ src/main/java/cn/myzqu/ygmall/service/impl/OrderDetailServiceImpl.java
@@ -1,5 +1,7 @@
 package cn.myzqu.ygmall.service.impl;
 
+import cn.myzqu.ygmall.dao.OrderDetailMapper;
+import cn.myzqu.ygmall.dao.OrderMapper;
 import cn.myzqu.ygmall.pojo.OrderDetail;
 import cn.myzqu.ygmall.service.OrderDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +17,11 @@ public class OrderDetailServiceImpl implements OrderDetailService{
 
 
     @Autowired
-    private OrderDetailService orderDetailService;
+    private OrderDetailMapper orderDetailMapper;
 
     @Override
     public List<OrderDetail> selectByOrderId(String orderId) {
-        List<OrderDetail> orderDetails = orderDetailService.selectByOrderId(orderId);
+        List<OrderDetail> orderDetails = orderDetailMapper.selectByOrderId(orderId);
         if(orderDetails.size()>0){
             return orderDetails;
         }
