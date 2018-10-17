3b5896a990c22514377e8a4b6906c70c4c85f626
your name <your email>
2018-10-17 23:52:24 +0800

    修复bug:解决无法插入追评回复；解决点击有追评回复的评论后再点击没有追评的评论却出现追评的bug

--- src/main/java/cn/myzqu/ygmall/dao/CommentMapper.java
+++ src/main/java/cn/myzqu/ygmall/dao/CommentMapper.java
@@ -2,6 +2,7 @@ package cn.myzqu.ygmall.dao;
 
 import cn.myzqu.ygmall.dto.CommentDTO;
 import cn.myzqu.ygmall.pojo.Comment;
+import cn.myzqu.ygmall.vo.CommentGoodsVO;
 import cn.myzqu.ygmall.vo.CommentReplyVO;
 import org.apache.ibatis.annotations.Param;
 
@@ -30,4 +31,6 @@ public interface CommentMapper {
     int updateStatusById(CommentDTO record);
 
     List<CommentReplyVO> selectByGoodsId(@Param("goodsId") String goodsId, @Param("goodsScore") Byte goodsscore);
+
+    List<CommentGoodsVO> selectAddCom(String userId);
 }
